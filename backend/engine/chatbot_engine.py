2caeeca51110128bf71c86d6120205160316b62a
Gurjinder <dev.gurjinder@gmail.com>
2025-05-05 12:34:45 +0100

    Task3 working setup

--- backend/engine/chatbot_engine.py
+++ backend/engine/chatbot_engine.py
@@ -2,10 +2,25 @@
 from experta import *
 
 import warnings
+import json
+from typing import Dict
 
 warnings.filterwarnings('ignore')
 from .experta_response import ExpertaResponse
+SENTENCES_FILE = "engine/sentences.json"
 
+
+def load_intentions(file_path: str) -> Dict:
+    """Load and parse the intentions JSON file.
+
+    Args:
+        file_path (str): Path to the intentions JSON file
+
+    Returns:
+        Dict: Parsed intentions data
+    """
+    with open(file_path, "r", encoding="utf-8") as f:
+        return json.load(f)
 """
 Expert system 
     The aim of the expert system is to take knowledge from a human expert and convert this into a number of
@@ -39,7 +54,8 @@ Experta:
 final_chatbot = False
 response = ExpertaResponse()
 answer = ''
-
+sentences = load_intentions(SENTENCES_FILE)
+# print(sentences)
 
 class Greeting(Fact):  # define fact
     ''' Info about the booking ticket'''
@@ -50,6 +66,14 @@ class Greeting(Fact):  # define fact
 class Book(Fact):
     pass
 
+class Task3(Fact):
+    pass
+
+class Contingency(Fact):
+    station_one = Field(str, mandatory=True)
+    station_two = Field(str, mandatory=True)
+    type = Field(str, mandatory=True)
+
 class TrainBot(KnowledgeEngine):
 
     @DefFacts()
@@ -57,12 +81,17 @@ class TrainBot(KnowledgeEngine):
         set_response('I am not sure how to respond to this question.')
         yield Fact(Greeting="I am not sure how to respond")
 
+        # for fact in sentences:
+        #     print(fact)
+        #     yield Fact(Contingency=fact)
+
     @Rule(Greeting(input='greet'))
     def greet_back(self):
         # print("BOT: you have selected a one way ticket. Have a good trip.")
         # if final_chatbot:
         #     print("BOT: If you don't have any other question, you can type bye.")
         response.set_engine_response('Hi, thanks for checking in. How can I help you?')
+        # find_response("colchester-manningtree-partial")
         # return 'Hi, thanks for checking in.'
 
 
@@ -131,15 +160,54 @@ class TrainBot(KnowledgeEngine):
     def say_sorry(self):
         set_response("Sorry, I have missed some of the important info.")
 
+
+    @Rule(Task3(input='incident'))
+    def say_incident(self):
+        set_response("Sure. What is the location of the incident?")
+
+    @Rule(Task3(input='location'))
+    def say_location(self):
+        set_response("Noted. Has it caused partial or full line blockage")
+
+    @Rule(Task3(input='blockage'))
+    def say_blockage(self):
+        set_response("Understood. What would be the approximate time of incident?")
+
+    @Rule(Task3(input='blockage_time'))
+    def say_blockage(self):
+        set_response("Understood. Please wait while I generate a response.")
+
+    # @Rule(Contingency("colchester","manningtree",Contingency(input="partial")))
+    # def say_partial_1(self):
+    #     set_response("colchester-manningtree-partial")
+    #     find_response("colchester-manningtree-partial")
+
+    @Rule(Contingency(station_one='colchester', station_two='manningtree', type='partial'))
+    def say_partial_cm(self):
+        set_response(find_response("colchester-manningtree-partial"))
+        # find_response("colchester-manningtree-partial")
+
+    @Rule(Contingency(station_one='colchester', station_two='manningtree', type='full'))
+    def say_full_cm(self):
+        set_response(find_response("colchester-manningtree-full"))
+
+
 def engine_response(user_input):
     engine = TrainBot()
     engine.reset()
-
-    if user_input is not None:
-        print('Clllled')
-        engine.declare(Greeting(input=user_input),Book(input=user_input))
-        # engine.declare(Book(user_input))   # adds a new fact to the list of factlist
+    # word =
+    # up = ''
+    if 'contingency' in user_input:
+        up = user_input.split('-')
+        engine.declare(Contingency(station_one=up[1], station_two=up[2], type=up[3]))
         engine.run()
+    else:
+        if user_input is not None:
+
+            engine.declare(Greeting(input=user_input),Book(input=user_input), Task3(input=user_input))
+            # engine.declare(Book(user_input))   # adds a new fact to the list of factlist
+            # engine.declare(Contingency(station_one=up[1], station_two=up[2], type=up[3]))
+            engine.run()
 
         return True
 
@@ -150,4 +218,36 @@ def set_response(current_response):
     response.set_engine_response(current_response)
 
 
+def find_response(user_input):
+    # user_input_lower = user_input.lower()
+    # for intent, data in sentences.items():
+    #     if any(pattern in user_input_lower for pattern in data["patterns"]):
+    #         return intent, 1.0
+    triggers = user_input.split('-')
+    print(sentences[triggers[0]+'-'+triggers[1]][triggers[2]])
+    plan = sentences[triggers[0]+'-'+triggers[1]][triggers[2]]
+    response = ''
+    for key, value in plan.items():
+        response += '<b>'+key+'</b>' + ' : ' + value + '<br>'
 
+
+    return response
+    # for word in user_input.split(' '):
+    #     for type_of_intention in sentences[user_input]:
+    #         if word.lower() in sentences[type_of_intention]["patterns"]:
+    #             # print(sentences[type_of_intention]['responses'])
+    #             # for key, values in sentences[type_of_intention]:
+    #             responses = sentences[type_of_intention]["responses"]
+    #             print(type(responses))
+    #             for key, value in responses.items():
+    #                 print("LOOOOOKKKKK ", key, value)
+    #                 # if values == "responses":
+                    #
+                    #     for value in values:
+                    #         print(value)
+
+                # Do not change these lines
+                # if type_of_intention == 'greeting' and final_chatbot:
+                #     print("BOT: We can talk about the time, date, and train tickets.\n(Hint: What time is it?)")
+                # return type_of_intention
+    # return None
